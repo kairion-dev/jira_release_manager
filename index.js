e93a0ce1217484635b0103d6dfe48cd59bca10d4
attrib <github@attrib.org>
2016-02-14 13:51:02 +0100

    Refactoring

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var
   kcommon = require('./lib/common.js'),
   Datastore = Promise.promisifyAll(require('nedb')),
   JiraApi = require('./lib/jira.js').Jira,
-  Git = require("nodegit"),
+  Git = require("./lib/git.js"),
   fs = require('fs'),
   db = {
     tags: Promise.promisifyAll(new Datastore({ filename: './tags', autoload: true })),
@@ -19,105 +19,16 @@ var
       access_token: '',
       access_token_secret: ''
     },
-    db: db.tickets
+    db: db
+  }),
+  git = Git.GitHistory({
+    git_path: '/home/attrib/kairion/kairion.git',
+    git_name: 'kairion',
+    db: db
   });
 
-// @todo: are there better ways for this globals?
-var globalRepo, knownTags = {};
-
-db.tags
-  .findAsync({})
-  .then((docs) => {
-    return Promise.map(docs, (tag, i, total) => {
-      knownTags[tag.tag] = tag;
-    })
-  })
-  // Open the repository directory.
-  .then(() => {return Git.Repository.openBare("/home/attrib/kairion/kairion.git")})
-  //.then(function(repo) {
-  // @todo: auth
-  //  return repo.fetch('origin');
-  //})
-  // Get list of tags
-  .then((repo) => {
-    globalRepo = repo;
-    return Git.Tag.list(repo);
-  })
-  // Retrieve the Tickets for each tag
-  .then((tagList) => {
-    var releaseTags = [];
-    tagList.forEach(function(tag) {
-      if (tag.match(/^\d+\.\d+\.\d+/)) {
-        releaseTags.push(tag);
-      }
-    });
-    releaseTags.sort().reverse();
-    return Promise.map(releaseTags, (tag, i, total) => {
-      // ignore already processed tags
-      if (i == total || knownTags[tag]) {
-        return;
-      }
-      var commit1;
-      // First the commit referenced by the tag is needed
-      return globalRepo.getTagByName(tag)
-        .then(function(tag) {
-          return Git.Commit.lookup(tag.owner(), tag.targetId());
-        })
-        .then(function (commit) {
-          commit1 = commit;
-          return globalRepo.getTagByName(releaseTags[i + 1]);
-        })
-        // Then the commit reference by the tag before is needed
-        .then(function(tag) {
-          return Git.Commit.lookup(tag.owner(), tag.targetId());
-        })
-        // Walking through the history and get all tickets in the commit message
-        .then(function (commit) {
-          var tickets = [];
-          var revwalk = commit1.owner().createRevWalk();
-          revwalk.sorting(Git.Revwalk.SORT.TIME);
-          revwalk.push(commit1.id());
-
-          return revwalk.getCommitsUntil(
-            function(c) {
-              // currently I use the date to break. Somehow waiting till the sha matches results in all commits
-              // Probably because of the merging shit.
-              // @todo: This means we lose all tickets which were worked on in a feature branch before the release
-              if (!c || c.date() <= commit.date()) {
-                return false;
-              }
-              var result = c.message().match(/^(K..?-\d+)/);
-              if (result && result.length > 1) {
-                if (result[1] == 'KD-0') {
-                  tickets.push(c.message().trim());
-                }
-                else {
-                  tickets.push(result[1]);
-                }
-              }
-              return true;
-            })
-            // save all tags with their tickets
-            .then(function(commits) {
-              tickets = kcommon.uniqueArray(tickets);
-              tickets.sort();
-              var result = {tag: tag, tickets: tickets, commits: commits.length};
-              return new Promise((resolve, reject) => {
-                db.tags.update({tag: tag}, {$set: result}, {upsert: true}, (err, numUpdated) => {
-                  if (err) reject(err);
-                  else resolve(result);
-                });
-              });
-            })
-            .catch(function(e) {
-              console.log('Error walking though the history of tag ' + tag + ': ' + e);
-            });
-        })
-        .catch(function(e) {
-          console.log('Error processing tag ' + tag + ': ' + e);
-        });
-    });
-  })
+git
+  .initialize()
   .then((tags) => {
     console.log('Processed all tags.');
 //    console.log(tags);

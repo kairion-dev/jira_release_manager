6abb593d1fcc57bf45161104e7ecaca4047c74ff
Manuel Wick <manuelwick@gmx.de>
2016-06-10 17:37:44 +0200

    KD-10805 webhook tries to init timetracking for a parent by checking its subtickets estimated time

--- webhooks/tickets-to-development.js
+++ webhooks/tickets-to-development.js
@@ -8,8 +8,6 @@ var
   log = require('../lib/logger.js'),
   Webhook = require('./abstract-webhook');
 
-var core = new Core();
-
 class TicketsToDevelopment extends Webhook {
 
   /**
@@ -46,6 +44,7 @@ class TicketsToDevelopment extends Webhook {
       });
   }
 
+
   /**
    * Get all children of the epic and set them to 'Selected for Development' if still not planned
    * 
@@ -62,18 +61,24 @@ class TicketsToDevelopment extends Webhook {
         return Promise.map(res.issues, (issue) => {
           // only move those children to 'Selected for Development' that are not planned
           if (issue.fields.status.id == this.statusNotPlanned ) {
-            return this.doTransition(issue.key, {
-              transition: {
-                id: this.transitionId
-              }
-            })
-              .catch((e) => log.warn("Could not move ticket '" + issue.key + "' to 'Selected for Development'"));
+            return this.doTransition(issue.key, { transition: { id: this.transitionId } } )
+              .catch((e) => {
+                return this.tryToInitTimeEstimate(issue.key)
+                  .catch((e) => log.warn("Could not move ticket '" + issue.key + "' to 'Selected for Development': " + e));
+              });
           }
         });
       });
   }
 
 
+  /**
+   * Check if a ticket of type epic is moved from planned to selected for development
+   * @param  {Jira Callback} data
+   * @return {Promise{Boolean}}
+   *   true, if the ticket is an epic and correctly moved
+   *   false, otherwise
+   */
   epicMovedFromNotPlannedToDevelopment(data) {
     return Promise.filter(data.changelog.items, (item) => {
       return item.field == 'status';
@@ -86,6 +91,76 @@ class TicketsToDevelopment extends Webhook {
 
 
   /**
+   * Try to init the estimated and remaining time by checking the subticket's estimated time.
+   * If there are estimated subtickets, time of the parent ticket will be set to 0 and the ticket will be moved.
+   * 
+   * @param  {String} issueKey
+   * @return {Promse}
+   */
+  tryToInitTimeEstimate(issueKey) {
+    return this.findIssue(issueKey)
+      .then((res) => {
+        // if we have no time estimated but subtickets
+        if (res && res.fields.timeestimate == null && res.fields.subtasks.length > 0) {
+          // get keys of all subtickets
+          var issueKeys = res.fields.subtasks.map((subtask) => subtask.key);
+          return this.getTimeEstimates(issueKeys)
+            .then((res) => {
+              // sum up estimated time of all subtickets
+              return Promise.reduce(res.issues, (total, issue) => {
+                return total + issue.fields.timeestimate;
+              }, 0);
+            });
+        }
+      })
+      .then((timeEstimated) => {
+        if (timeEstimated) {
+          return this.initTimetracking(issueKey)
+            .then(() => this.doTransition(issueKey, { transition: { id: this.transitionId } } ))
+            .catch((e) => Promise.reject('Estimated and remaining time set but still failing to set status'))
+        } else {
+          return Promise.reject('No subticket or none of them has estimated time');
+        }
+      });
+  }
+
+
+  /**
+   * Find a Jira issue.
+   * 
+   * @param  {String} issueKey
+   *   e.g. 'KD-10790'
+   * @return {Promise{JiraIssue}}
+   */
+  findIssue(issueKey) {
+    return this.jira.jira.findIssueAsync(issueKey);
+  }
+
+
+  /**
+   * Get time estimates for the given issues.
+   * 
+   * @param  {Array{String}} issueKeys
+   *   e.g. ['KD-10790', 'KD-8997']
+   * @return {Promise{Array{JiraIssue}}}
+   */
+  getTimeEstimates(issueKeys) {
+    return this.jira.jira.searchJiraAsync('key in (' + issueKeys.toString() + ')', { fields: ['timeestimate'] });
+  }
+
+
+  /**
+   * Set the estimated and remaining time of the ticket with issueKey to 0h.
+   * 
+   * @param  {String} issueKey
+   * @return {Promise}
+   */
+  initTimetracking(issueKey) {
+    return this.jira.jira.updateIssueAsync(issueKey, { update: { timetracking: [{ set: { originalEstimate: '0h', remainingEstimate: '0h' } }] } });
+  }
+
+
+    /**
    * Get all children of the epic with the given issueKey.
    * 
    * @param  {String} issueKey
@@ -95,8 +170,9 @@ class TicketsToDevelopment extends Webhook {
    */
   getAllEpicChildren(issueKey) {
     var options = { fields: ['status'] };
-    return core.jira.jira.searchJiraAsync('parent in tempoEpicIssues('+issueKey+') OR "Epic Link" in ('+issueKey+') ORDER BY issuetype ASC, updatedDate ASC', options);
+    return this.jira.jira.searchJiraAsync('parent in tempoEpicIssues('+issueKey+') OR "Epic Link" in ('+issueKey+') ORDER BY issuetype ASC, updatedDate ASC', options);
   }
+
 
   /**
    * Do a status transition for the given issue.
@@ -110,6 +186,7 @@ class TicketsToDevelopment extends Webhook {
   }
 
 
+  
 }
 
 module.exports = TicketsToDevelopment;

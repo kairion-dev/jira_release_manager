fd186b375cd5f370ec7cf11b976df97323127729
Manuel Wick <manuelwick@gmx.de>
2016-06-24 11:31:25 +0200

    KD-10971 basic tests for webhook details logic added

--- tests/webhooks/webhook-engine.js
+++ tests/webhooks/webhook-engine.js
@@ -9,7 +9,7 @@ var
   helper = require('../helper/common.js');
 
 var webhookPaths = {
-  'helloWorld' : { path: '../tests/webhooks/webhooks/hello-world'},
+  'hello_world' : { path: '../tests/webhooks/webhooks/hello-world'},
   'calc_add' : { path: '../tests/webhooks/webhooks/calculator', params: { operation: function(a,b) { return a + b } }}
 }
 
@@ -31,12 +31,12 @@ describe('Testing Webhook Engine', function() {
         });
     });
     it('Webhook engine should also work fine by passing webhooks via constructor config', function() {
-      let config = { 'helloWorld': webhookPaths.helloWorld };
+      let config = { 'hello_world': webhookPaths.hello_world };
       return engine.registerByConfig(config)
         .then(() => engine.invoke('Hello World by config'))
         .then((res) => {
           Object.keys(res.webhookResults).should.have.lengthOf(1);
-          res.webhookResults['helloWorld'].result.should.equal('Hello World by config');
+          res.webhookResults['hello_world'].result.should.equal('Hello World by config');
         });
     });
   });
@@ -60,7 +60,7 @@ describe('Testing Webhook Engine', function() {
         });
     });
     it('Engine should work fine with multiple webhooks of different types', function() {
-      let config = { 'helloWorld': webhookPaths.helloWorld, 'calc_add': webhookPaths.calc_add };
+      let config = { 'hello_world': webhookPaths.hello_world, 'calc_add': webhookPaths.calc_add };
       let Calculator = require('./webhooks/calculator');
       let calcMult = new Calculator('calc_mult', { operation: function(a,b) { return a * b; } });
 
@@ -71,10 +71,88 @@ describe('Testing Webhook Engine', function() {
         .then(() => engine.invoke(request))
         .then((res) => {
           Object.keys(res.webhookResults).should.have.lengthOf(3); // we expect results from both webhooks
-          res.webhookResults['helloWorld'].result.should.deep.equal(request);
+          res.webhookResults['hello_world'].result.should.deep.equal(request);
           res.webhookResults['calc_add'].result.should.equal(8); // 5 + 3
           res.webhookResults['calc_mult'].result.should.equal(15); // 5 * 3
         })
     });
   });
+  describe('Webhook Details', function() {
+    it('Should have well-formed structure', function() {
+      return db.webhooks.removeAsync({}, { multi: true })
+        .then(() => engine.registerByConfig(webhookPaths))
+        .then(() => engine.invoke({ a: 4 , b: 2 }))
+        .then(() => engine.getWebhooksData())
+        .then((res) => {
+          Object.keys(res).should.have.lengthOf(2);
+          res['hello_world'].should.have.property('params');
+          res['hello_world'].should.have.property('data');
+          res['hello_world'].data.should.have.property('invoked');
+          res['hello_world'].data.should.have.property('errors');
+          res['hello_world'].data.should.have.property('last_time_invoked');
+        })
+    });
+    it('Should have correct number of invokes and errors', function() {
+      let Calculator = require('./webhooks/calculator');
+      let calcFail = new Calculator('calc_fail', { });
+      let timestamp1, timestamp2;
+      return db.webhooks.removeAsync({}, { multi: true })
+        .then(() => engine.registerByConfig(webhookPaths))
+        .then(() => engine.invoke({ a: 4 , b: 2 }))
+        .then((res) => { timestamp1 = new Date(res.timestamp).toString(); })
+        .then(() => engine.getWebhooksData())
+        // both webhooks should have been invoked successfully
+        .then((res) => {
+          Object.keys(res).should.have.lengthOf(2);
+          res['hello_world'].data.invoked.should.equal(1);
+          res['hello_world'].data.errors.should.equal(0);
+          res['hello_world'].data.last_time_invoked.should.equal(timestamp1);
+          res['calc_add'].data.invoked.should.equal(1);
+          res['calc_add'].data.errors.should.equal(0);
+          res['calc_add'].data.last_time_invoked.should.equal(timestamp1);
+        })
+        // 'hello_world' webhook should not be invoked, but 'calc_add'
+        .then(() => {
+          return new Promise((resolve, reject) => {
+            // just wait for it ...
+            setTimeout(function() {
+              engine.invoke('dont execute')
+                .then((res) => {
+                  // save new timestamp that is at least one second higher
+                  timestamp2 = new Date(res.timestamp).toString();
+                  resolve();
+                })
+            }, 1000);
+          })
+        })
+        .then(() => engine.getWebhooksData())
+        .then((res) => {
+          Object.keys(res).should.have.lengthOf(2);
+          res['hello_world'].data.invoked.should.equal(1);
+          res['hello_world'].data.errors.should.equal(0);
+          // 'hello_world' still should have the old timestamp cause it was not invoked again
+          res['hello_world'].data.last_time_invoked.should.equal(timestamp1);
+          res['calc_add'].data.invoked.should.equal(2);
+          res['calc_add'].data.errors.should.equal(0);
+          // 'calc_add' should have the new timestamp
+          res['calc_add'].data.last_time_invoked.should.equal(timestamp2);
+        })
+        // now we add another calc webhook that has no operation and thus will fail miserably
+        .then(() => engine.register(calcFail))
+        .then(() => engine.invoke({ a: 3 , b: 3 }))
+        .then(() => engine.getWebhooksData())
+        .then((res) => {
+          // we should have data for 3 webhooks now
+          Object.keys(res).should.have.lengthOf(3);
+          // both old webhooks should be executed as usual
+          res['hello_world'].data.invoked.should.equal(2);
+          res['hello_world'].data.errors.should.equal(0);
+          res['calc_add'].data.invoked.should.equal(3);
+          res['calc_add'].data.errors.should.equal(0);
+          // the new 'calc_fail' webhook should fail due to the missing operation function that is called nonetheless
+          res['calc_fail'].data.invoked.should.equal(1);
+          res['calc_fail'].data.errors.should.equal(1);
+        })
+    });
+  })
 });
